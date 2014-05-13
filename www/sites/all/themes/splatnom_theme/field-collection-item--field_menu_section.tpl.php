3914547f1b4e07971100a44b01dab97cfdb7c3a9
henry <hleong25@gmail.com>
2014-05-13 01:28:42 -0700

    Add navigation to menus

--- www/sites/all/themes/splatnom_theme/field-collection-item--field_menu_section.tpl.php
+++ www/sites/all/themes/splatnom_theme/field-collection-item--field_menu_section.tpl.php
@@ -27,8 +27,15 @@
  * @see template_preprocess_entity()
  * @see template_process()
  */
+
+$section = $content['field_menu_section_title'];
+$section_id = $section['#object']->item_id;
+$section_title = $section['#items'][0]['value'];
+
+$section_anchor = "$section_id-$section_title";
+
 ?>
-<div class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+<div id="<?php print $section_anchor; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
   <div class="content"<?php print $content_attributes; ?>>
     <?php print render($content); ?>
   </div>
